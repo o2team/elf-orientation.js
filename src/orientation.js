965a2bb4d93bbe1671f2d66332d5298907e7f81b
Liucw <liucw.mail@gmail.com>
2016-11-08 19:22:05 +0800

    fix: avoid repeat listen orientation event

--- src/orientation.js
+++ src/orientation.js
@@ -1,4 +1,4 @@
-(function(root, factory) {
+(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD
         define([], factory)
@@ -9,124 +9,135 @@
         // Browser globals (root is window)
         root.Orientation = factory()
     }
-}(this, function() {
-    var assign = Object.assign || function(base, extend) {
+}(this, function () {
+    var assign = Object.assign || function (base, extend) {
         for (var prop in extend) {
             base[prop] = extend[prop]
         }
         return base
     }
 
-    var Orientation = function(options) {
-        var opts = {
-            onChange: function() {}
+    var Orientation = function (options) {
+        this.opts = {
+            onChange: function () {}
         }
         if (typeof options === 'function') {
-            opts.onChange = options
+            this.opts.onChange = options
         } else {
-            opts = assign(opts, options)
+            this.opts = assign(this.opts, options)
         }
 
-        var data = {
+        this.data = {
             initLeftRotate: undefined,
-            initForwardSlant: opts.initForwardSlant,
+            initForwardSlant: this.opts.initForwardSlant,
             orientation: window.orientation || 0
         }
 
         // iOS detection from: http://stackoverflow.com/a/9039885/177710
-        data.isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+        this.data.isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
 
-        this.handleDeviceOrientation = function(e) {
+        this.handleDeviceOrientation = function (e) {
             var alpha = Math.floor(e.alpha)
             var beta = Math.floor(e.beta)
             var gamma = Math.floor(e.gamma)
 
             // https://github.com/w3c/deviceorientation/issues/6
-            if (data.initLeftRotate === undefined) {
-              data.initLeftRotate = data.isIOS ? Math.floor(e.webkitCompassHeading) : alpha
+            if (this.data.initLeftRotate === undefined) {
+                this.data.initLeftRotate = this.data.isIOS ? Math.floor(e.webkitCompassHeading) : alpha
             }
             // iOS 的 alpha 的已经是相对的了，不需要做处理
-            if (!data.isIOS) {
-              if (alpha > 0 || alpha < data.initLeftRotate) {
-                alpha += 360
-              }
-              alpha -= data.initLeftRotate
+            if (!this.data.isIOS) {
+                if (alpha > 0 || alpha < this.data.initLeftRotate) {
+                    alpha += 360
+                }
+                alpha -= this.data.initLeftRotate
             }
 
-            data.leftRotate = alpha < 180 ? alpha : alpha - 360
-            data.rightRotate = -data.leftRotate
+            this.data.leftRotate = alpha < 180 ? alpha : alpha - 360
+            this.data.rightRotate = -this.data.leftRotate
 
-            switch (data.orientation) {
+            switch (this.data.orientation) {
                 case 0:
-                    data.forwardSlant = beta
+                    this.data.forwardSlant = beta
                     break
                 case 90:
-                    data.forwardSlant = gamma < 0 ? -gamma : 180 - gamma
+                    this.data.forwardSlant = gamma < 0 ? -gamma : 180 - gamma
                     break
                 case -90:
-                    data.forwardSlant = gamma < 0 ? 180 + gamma : gamma
+                    this.data.forwardSlant = gamma < 0 ? 180 + gamma : gamma
                     break
             }
-            data.backwardSlant = -data.forwardSlant
-            data.isForward = data.forwardSlant > 0
-            data.isBackward = !data.isForward
-            // 记录初始的 向前倾斜度
-            if (data.initForwardSlant === undefined) data.initForwardSlant = data.forwardSlant
-            switch (data.orientation) {
+            this.data.backwardSlant = -this.data.forwardSlant
+            this.data.isForward = this.data.forwardSlant > 0
+            this.data.isBackward = !this.data.isForward
+                // 记录初始的 向前倾斜度
+            if (this.data.initForwardSlant === undefined) this.data.initForwardSlant = this.data.forwardSlant
+            switch (this.data.orientation) {
                 case 0:
-                    data.forwardThreshold = data.initForwardSlant > 0 ? -180 + data.initForwardSlant : 180 + data.initForwardSlant
-                    if (data.initForwardSlant > 0 && beta < data.forwardThreshold) {
-                        data.relativeForwardSlant = beta + 360 - data.initForwardSlant
-                    } else if (data.initForwardSlant < 0 && beta > data.forwardThreshold) {
-                        data.relativeForwardSlant = beta - 180 - data.initForwardSlant
+                    this.data.forwardThreshold = this.data.initForwardSlant > 0 ? -180 + this.data.initForwardSlant : 180 + this.data.initForwardSlant
+                    if (this.data.initForwardSlant > 0 && beta < this.data.forwardThreshold) {
+                        this.data.relativeForwardSlant = beta + 360 - this.data.initForwardSlant
+                    } else if (this.data.initForwardSlant < 0 && beta > this.data.forwardThreshold) {
+                        this.data.relativeForwardSlant = beta - 180 - this.data.initForwardSlant
                     } else {
-                        data.relativeForwardSlant = beta - data.initForwardSlant
+                        this.data.relativeForwardSlant = beta - this.data.initForwardSlant
                     }
                     break
                 case 90:
                 case -90:
-                    // data.forwardThreshold
+                    // this.data.forwardThreshold
                     // 横屏时，只考虑向前倾斜度在 [0, 180] 的情况
-                    data.relativeForwardSlant = data.forwardSlant - data.initForwardSlant
+                    this.data.relativeForwardSlant = this.data.forwardSlant - this.data.initForwardSlant
                     break
             }
-            data.relativeBackwardSlant = -data.relativeForwardSlant
-            data.isRelativeForward = data.relativeForwardSlant > 0
-            data.isRelativeBackward = !data.isRelativeForward
+            this.data.relativeBackwardSlant = -this.data.relativeForwardSlant
+            this.data.isRelativeForward = this.data.relativeForwardSlant > 0
+            this.data.isRelativeBackward = !this.data.isRelativeForward
 
-            switch (data.orientation) {
+            switch (this.data.orientation) {
                 case 0:
-                    data.leftSlant = -gamma
+                    this.data.leftSlant = -gamma
                     break
                 case 90:
-                    data.leftSlant = -beta
+                    this.data.leftSlant = -beta
                     break
                 case -90:
-                    data.leftSlant = beta
+                    this.data.leftSlant = beta
                     break
             }
-            data.rightSlant = -data.leftSlant
-            data.isLeft = data.leftSlant > 0
-            data.isRight = !data.leftSlant
+            this.data.rightSlant = -this.data.leftSlant
+            this.data.isLeft = this.data.leftSlant > 0
+            this.data.isRight = !this.data.leftSlant
 
-            opts.onChange(assign(e, data))
+            this.opts.onChange(assign(e, this.data))
         }
 
-        this.handleOrientationChange = function(e) {
-            data.orientation = window.orientation
-            data.initForwardSlant = undefined
+        this.handleOrientationChange = function (e) {
+            this.data.orientation = window.orientation
+            this.data.initForwardSlant = undefined
         }
 
     }
 
-    Orientation.prototype.init = function() {
-        window.addEventListener('deviceorientation', this.handleDeviceOrientation, false)
-        window.addEventListener('orientationchange', this.handleOrientationChange, false)
+    Orientation.prototype.init = function () {
+        // 防止重复监听
+        if (this._isListeningOrientation) {
+            window.removeEventListener('deviceorientation', this._handleDeviceOrientation, false)
+            window.removeEventListener('orientationchange', this._handleOrientationChange, false)
+        }
+        this._isListeningOrientation = true
+        this.data.initLeftRotate = undefined
+        this.data.initForwardSlant = this.opts.initForwardSlant
+        this._handleDeviceOrientation = this.handleDeviceOrientation.bind(this)
+        this._handleOrientationChange = this.handleOrientationChange.bind(this)
+        window.addEventListener('deviceorientation', this._handleDeviceOrientation, false)
+        window.addEventListener('orientationchange', this._handleOrientationChange, false)
     }
 
-    Orientation.prototype.destory = function() {
-        window.removeEventListener('deviceorientation', this.handleDeviceOrientation, false)
-        window.removeEventListener('orientationchange', this.handleOrientationChange, false)
+    Orientation.prototype.destory = function () {
+        this._isListeningOrientation = false
+        window.removeEventListener('deviceorientation', this._handleDeviceOrientation, false)
+        window.removeEventListener('orientationchange', this._handleOrientationChange, false)
     }
 
     return Orientation
